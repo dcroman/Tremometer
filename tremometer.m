c3d84bad8d5e19994587ccfc80d103ec26b0de8c
Diana Roman <droman@carnegiescience.edu>
2017-07-31 14:25:06 -0400

    Add files via upload

---
+++ tremometer.m
@@ -0,0 +1,103 @@
+function [harmPow, harmFreq, interharmPow, harmStrength] = tremometer(x,fs,nHarm)
+
+ 
+% use Kaiser window to reduce effects of leakage before calculating periodogram
+x = x - mean(x);
+n = length(x);
+w = kaiser(n,38);	    
+[Pxx, F] = periodogram(x,w,n,fs);           %returns the PSD estimate - the integral of the PSD is the power in the time series
+
+
+% pre-allocate harmonic tables - nHarm is a user-supplied argument - compute HSI for nHarm harmonics
+harmPow = NaN(nHarm,1);
+harmFreq = NaN(nHarm,1);
+interharmPow = NaN(nHarm-1,1);
+harmStrength = NaN(nHarm-1,1);
+harmIdx = NaN(nHarm, 2);	
+iHarm = NaN(nHarm, 1);
+
+% Remove DC component
+Pxx(1) = 2*Pxx(1);				
+rbw = enbw(w, fs);				
+[~, ~, ~, iLeft, iRight] = signal.internal.getToneFromPSD(Pxx, F, rbw, 0);  
+Pxx(iLeft:iRight) = 0;                                                       
+
+
+% Get an estimate of the fundamental frequency / amplitude through Harmonic Product Spectrum calculation
+% hps1 is Pxx, so there are really only five decimations here. 
+hps1=downsample(Pxx,1);                     %Redundant - could just use Pxx
+hps2=downsample(Pxx,2);
+hps3=downsample(Pxx,3);
+hps4=downsample(Pxx,4);
+hps5=downsample(Pxx,5);
+hps6=downsample(Pxx,6);
+
+y=[];
+for i=1:length(hps6)
+  Product=hps1(i)*hps2(i)*hps3(i)*hps4(i)*hps5(i)*hps6(i);
+  y(i)=Product;
+end
+
+[~,n]=findpeaks(y,'SORTSTR','descend');
+Maximum=n(1);
+Ffund=F(Maximum);
+
+	
+%Check Pxx for peak near the ID'd fundamental freq
+[~, ~, iHarm(1), iLeft, iRight] = signal.internal.getToneFromPSD(Pxx, F, rbw, Ffund);
+harmFreq(1)=F(iHarm(1));
+harmPow(1)=Pxx(iHarm(1));
+harmIdx(1, :) = [iLeft; iRight];
+
+% get indices of the harmonic frequencies / amplitudes, then use to query frequency and amplitude for each harmonic
+for i=2:nHarm
+  [~, ~, iHarm(i), iLeft, iRight] = signal.internal.getToneFromPSD(Pxx, F, rbw, i*Ffund);
+  harmFreq(i)=F(iHarm(i));
+  harmPow(i)=Pxx(iHarm(i));
+  harmIdx(i, :) = [iLeft; iRight];
+end
+
+%Adjust frequency and amplitude of candidate harmonics by finding the maximum amplitude within 0.1Hz of each F in Pxx. It needs the 'else' bit or it throws a subscript indices tantrum if iHarm indices are low. 
+%For now this is hardwired to +-0.1Hz search range.
+for i=1:nHarm
+    if iHarm(i)>=7
+harmPow(i) = max(Pxx(iHarm(i)-6:iHarm(i)+6));
+iHarm(i)=find(Pxx==harmPow(i));
+harmFreq(i)=F(iHarm(i));
+    else
+harmFreq(i)=F(iHarm(i));
+harmPow(i)=Pxx(iHarm(i));
+    end
+end
+
+
+% Compute inter-harmonic frequencies - do this by taking the max in between harmonics
+% First, must meet all three of the following conditions to assign an interharmonic, otherwise it gets a zero:
+% harmIdx has two values for each harmonic - for an interharmonic to be calculated all of the numbers in column 1 have to be unique and all of the numbers in column 2 have to be unique (first two conditions)
+% all of the numbers in harmIdx also have to be real numbers, not NANs (third condition)
+% This is a kludge and I should see if there's a better way to deal with it..........
+
+for i=1:(nHarm-1)
+    if length(unique(harmIdx(:,1)))==nHarm && length(unique(harmIdx(:,2)))==nHarm && ~any(isnan(harmIdx(:,1)))
+    interharmPow(i)=max(Pxx(round(harmIdx(i,2)+((harmIdx((i+1),1)-harmIdx(i,2))*.25)):round(harmIdx(i,2)+((harmIdx((i+1),1)-harmIdx(i,2))*.75))));
+    else interharmPow(i)=0;
+    end
+end
+
+if any(interharmPow)
+   interharmPow = interharmPow*rbw;
+end
+
+
+% compute harmonic strength index
+
+for i=1:(nHarm-1)
+    if any(interharmPow)
+        harmStrength(i)=harmPow(i)/interharmPow(i);
+    else
+        harmStrength(i)=0;
+    end
+end
+
+
+
---
+++ tremometer_control.m
@@ -0,0 +1,173 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Tremometer 1.0                                                                                    %
+%                                                                                                     %
+%   Matlab code to automatically detect and characterize harmonic tremor                              %
+%   in continuous seismic data using a pitch-detection algorithm                                      %
+%                                                                                                     %
+%   Diana C. Roman                                                                                    %
+%   Last modified July 31, 2017                                                                       %
+%                                                                                                     %
+%   Please cite: Roman, D.C. (2017), Automated detection and characterization of harmonic tremor      %
+%   in continuous seismic data. Geophys. Res. Lett.,44,doi: 10.1002/2017GL073715.                     %
+%                                                                                                     %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   This bit imports a day's worth         %
+%   of filtered and instrument-corrected   %
+%   data using Haney's code                %
+%                                          %
+%   Remove for public version              %                                                                                               *******************************
+%   IMPORTANT - make sure Haney plot       %
+%   is off if using this!                  %
+%                                          %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+rm_instrum_resp_ppp
+clear badvals digout digoutf fhi flo fsize idelay ordh ordl ovrsampl pols rawdata scrsz y zers
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Set the following variables            %
+%   (or leave as default values)           %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+x=prcdata;      		%Assign one day of (instrument-corrected and filtered) data to x
+yr=2014;			%Set start date (four digit year) of input data
+mo=12;				%Set start date (two digit month) of input data
+dy=02;				%Set start date (two digit day) of input data
+fs=100;          		%set sampling frequency (in Hz)
+minfreq=0.5;    		%set the minimum frequency of the fundamental (in Hz). Default is 0.5 Hz
+minHSI=30;       		%set the minimum Harmonic Strength Index for fundamental and two overtones. Default is 30. 
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Don't change anything below here       %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+nHarm=4;
+
+s=1440;
+
+for i=1:s
+startpt(i)=1+((i-1)*fs*60);
+endpt(i)=i*fs*60;
+end
+
+final=zeros(14,s);
+
+for j=1:s
+[harmpow,harmfreq,interharmpow,harmstrength]=tremometer(x(startpt(j):endpt(j)),fs,nHarm);
+final(:,j)=vertcat(harmfreq,harmpow,interharmpow,harmstrength);
+end
+
+final=final';
+
+
+%   Check minimum frequency criterion 
+for j=1:s
+if final(j,1) > minfreq
+final(j,15)=1;
+else final(j,15)=0;
+end
+end
+
+
+%   Check minimum HSI criterion for each harmonic  
+for j=1:s
+if final(j,12) > minHSI
+final(j,16)=1;
+else final(j,16)=0;
+end
+end
+
+for j=1:s
+if final(j,13) > minHSI
+final(j,17)=1;
+else final(j,17)=0;
+end
+end
+
+for j=1:s
+if final(j,14) > minHSI
+final(j,18)=1;
+else final(j,18)=0;
+end
+end
+
+
+%   Check if all criteria are met and flag tremor episodes, then add them to a list of detections with times
+for j=1:s
+if final(j,15)+final(j,16)+final(j,17)+final(j,18)==4
+final(j,19)=1;
+else final(j,19)=0;
+end
+end
+
+%Make a table of harmonic tremor detections
+ind1 = final(:,19) == 1;
+date=datetime(yr,mo,dy,0,0,0);
+t1=date;
+t2=date+minutes(1439);
+t=t1:minutes(1):t2;
+t=t';
+detection_times=t(ind1,:);
+fund_freq=final(ind1,1);
+harm_freq_1=final(ind1,2);
+harm_freq_2=final(ind1,3);
+harm_freq_3=final(ind1,4);
+harm_pow_1=final(ind1,5);
+harm_pow_2=final(ind1,6);
+harm_pow_3=final(ind1,7);
+harm_pow_4=final(ind1,8);
+interharm_pow_1=final(ind1,9);
+interharm_pow_2=final(ind1,10);
+interharm_pow_3=final(ind1,11);
+HSI_1=final(ind1,12);
+HSI_2=final(ind1,13);
+HSI_3=final(ind1,14);
+clc
+detections=table(detection_times,fund_freq,harm_freq_1,harm_freq_2,harm_freq_3,HSI_1,HSI_2,HSI_3)
+
+
+%   Make some plots  
+plot(t,final(:,12))
+datetick('x')
+ylabel('Harmonic Strength Index')
+xlabel('Time of Day')
+hline=refline([0 minHSI]);
+hline.Color = 'r';
+%end
+
+figure
+plot(detection_times,harm_freq_1, 'o')
+ylabel('Frequency (Hz)')
+xlabel('Time of Day')
+%end
+
+%Calibration mode - make Figure 1c for all detections
+for j=1:s
+if final(j,19) == 1
+figure
+snip=x(startpt(j):endpt(j));
+snip=snip-mean(snip);
+n=length(snip);
+w=kaiser(n,38);
+[Pxx,F]=periodogram(snip,w,n,fs);
+plot(F,Pxx)
+hold('on')
+plot(final(j,1:3),final(j,5:7),'*')
+ylabel('Power')
+xlabel('Frequency (Hz)')
+end
+end
+
+
+%tidy up
+clear date detection_times dy endpt F fs fund_freq harm_freq_1 harm_freq_2 harm_freq_3 harm_pow_1 harm_pow_2 harm_pow_3 harm_pow_4 harmfreq harmpow harmstrength hline HSI_1 HSI_2 HSI_3 i ind1 interharmpow interharm_pow_1 interharm_pow_2 interharm_pow_3 j minfreq minHSI mo n nHarm prcdata Pxx s samplrate snip startpt t t1 t2 temp_detections w x yr
+
